19e83907ac30c88d4f6119b51acf0ac46bb36bb2
Dennis Mwaniki <mwaniki.dennis@gmail.com>
2025-07-22 05:26:52 +0000

    Error: Map container is already initialized.     at NewClass._initContai

--- src/components/app/leaflet-map.tsx
+++ src/components/app/leaflet-map.tsx
@@ -1,6 +1,7 @@
 
 'use client';
 
+import { useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
